3c3eab389521370805e2cddeb8b48215fce26722
dimifcv <dimifcv@terra.com>
2019-02-16 10:17:23 +0100

    Se agregan 2 ficheros via upload files

---
+++ HolaGit/src/tk/com/eclipse01/git/HolaGit.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package tk.com.eclipse01.git;
+
+/**
+ * @author damian
+ * Ejemplo de integracion de Eclipse con Git
+ */
+public class HolaGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo Git!");
+	}
+
+}
